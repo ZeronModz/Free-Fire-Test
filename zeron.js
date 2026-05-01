c9d702c7ebe1492d37b3e7f8fb50bddf0ad9c04a
ZeronModz <zeronmodz@gmail.com>
2026-05-02 00:49:59 +0600

    Update zeron.js

--- zeron.js
+++ zeron.js
@@ -2,40 +2,52 @@
 
 (function () {
 
-  function start() {
+  function run() {
 
-    const div = document.createElement("div");
-    div.style.position = "fixed";
-    div.style.top = "0";
-    div.style.left = "0";
-    div.style.width = "100%";
-    div.style.height = "100%";
-    div.style.background = "black";
-    div.style.zIndex = "99999";
-    div.style.display = "flex";
-    div.style.justifyContent = "center";
-    div.style.alignItems = "center";
-    div.style.color = "#00ffcc";
-    div.style.fontSize = "30px";
-    div.style.fontFamily = "monospace";
+    // main overlay
+    const box = document.createElement("div");
+    box.style.position = "fixed";
+    box.style.top = "0";
+    box.style.left = "0";
+    box.style.width = "100%";
+    box.style.height = "100%";
+    box.style.background = "black";
+    box.style.zIndex = "999999";
+    box.style.display = "flex";
+    box.style.flexDirection = "column";
+    box.style.alignItems = "center";
+    box.style.justifyContent = "center";
+    box.style.color = "#00ffcc";
+    box.style.fontFamily = "monospace";
 
-    div.innerText = "Developer Xeron 🚀";
+    // text
+    const text = document.createElement("h1");
+    text.innerText = "Hacked By Xeron 😈";
+    text.style.fontSize = "24px";
+    text.style.opacity = "0";
+    text.style.transition = "1s";
 
-    document.body.appendChild(div);
+    box.appendChild(text);
+    document.body.appendChild(box);
 
+    // animation
     setTimeout(() => {
-      div.style.transition = "1s";
-      div.style.opacity = "0";
-      setTimeout(() => div.remove(), 1000);
-    }, 3000);
+      text.style.opacity = "1";
+    }, 500);
 
+    // auto remove
+    setTimeout(() => {
+      box.style.transition = "1s";
+      box.style.opacity = "0";
+      setTimeout(() => box.remove(), 1000);
+    }, 4000);
   }
 
-  // wait until page load
+  // auto run fix
   if (document.readyState === "loading") {
-    document.addEventListener("DOMContentLoaded", start);
+    document.addEventListener("DOMContentLoaded", run);
   } else {
-    start();
+    run();
   }
 
 })();
